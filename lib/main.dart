eedaa4025985fd6b69d37989cb30528e3acd5fd7
farid-maulana <faridmaulana1921@gmail.com>
2021-03-30 19:47:14 +0700

    TextField widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,61 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: MyLayout()),
-      ),
-    );
-  }
-}
-
-class MyLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: RaisedButton(
-        child: Text(
-          'Show alert',
-          style: TextStyle(
-            color: Colors.white,
+        appBar: AppBar(
+          title: Text("Contoh TextField"),
+        ),
+        body: TextField(
+          obscureText: false,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Nama',
           ),
         ),
-        onPressed: () {
-          showAlertDialog(context);
-        },
-        color: Colors.blue,
       ),
     );
   }
-}
-
-showAlertDialog(BuildContext context) {
-  // Buat button OK
-  Widget okButton = FlatButton(
-    onPressed: () {},
-    child: Text('OK'),
-  );
-
-  Widget cancelButton = FlatButton(
-    onPressed: () => Navigator.pop(context),
-    child: Text('Cancel'),
-  );
-
-  // Setup alert dialog
-  AlertDialog alertDialog = AlertDialog(
-    title: Text("Ini adalah judul"),
-    content: Text("Ini adalah isi pesan"),
-    actions: [
-      okButton,
-      cancelButton
-    ],
-  );
-
-  // Menampilkan alert dialog
-  showDialog(
-    context: context, 
-    builder: (BuildContext context) {
-      return alertDialog;
-    }
-  );
 }
